5a6b1360baba7c4508046afa952f9a9123ee320d
jgx <jules.gioux@gmail.com>
2020-06-22 01:18:57 +0200

    js ok

--- formNewStud.php
+++ formNewStud.php
@@ -4,29 +4,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Redirige l'utilisateur si Javascript est désactivé -->
+    <noscript>
+        <META HTTP-EQUIV="Refresh" CONTENT="0;URL=noScript.html">
+    </noscript>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="assets/css/myMaterialize.css">  
+    <!-- Compiled and minified JavaScript -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
+    <script>
+        //Création de la balise
+        let tt = document.createElement('script');   
+        function importJS(){          
+            document.getElementById('initMat').insertBefore(tt, document.getElementById('initMat')[0]);
+            tt.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js';
+        }
+    </script>
     <title>Partie 10 - TP-1</title>
 </head>
 
-<body>
+<body class="container">
 
     <section>
         <form action="" method="post">
             <fieldset>
                 <legend>Ajouter un apprenant :</legend>
                 <div class="row">
-                
+
                     <div class="input-field col s6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input type="text" name="lName" id="lName" class="validate" placeholder="John" min="2" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus required>
+                        <input type="text" name="lName" id="lName" class="validate" placeholder="John" min="2" max="50"
+                            maxlength="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus
+                            required>
                         <label for="lName">Nom</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
                     <div class="input-field col s6">
-                        <input type="text" name="fName" id="fName" class="validate" placeholder="Doe" min="2" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" required>
+                        <input type="text" name="fName" id="fName" class="validate" placeholder="Doe" min="2" max="50"
+                            maxlength="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" required>
                         <label for="fName">Prénom</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
@@ -34,7 +52,8 @@
                 <div class="row">
                     <div class="input-field col s12">
                         <i class="material-icons prefix">account_circle</i>
-                        <input type="text" class="datepicker" id="dOfB" name="dOfB" class="validate" placeholder="1989-12-23" pattern="([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))" required>
+                        <input type="text" class="datepicker validate" id="dOfB" name="dOfB" placeholder="19-06-2020"
+                            pattern="(0?[1-9]|[12][0-9]|3[01])[\-](0?[1-9]|1[012])[\-]\d{4}" required>
                         <label for="dOfB">Date de naissance</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
@@ -42,13 +61,17 @@
                 <div class="row">
                     <div class="input-field col s6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input type="text" name="country" id="country" class="validate" placeholder="France" min="2" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus required>
+                        <input type="text" name="country" id="country" class="validate" placeholder="France" min="2"
+                            max="50" maxlength="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}"
+                            autofocus required>
                         <label for="country">Pays de naissance</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
                     <div class="input-field col s6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input type="text" name="nationality" id="nationality" class="validate" placeholder="Française" min="2" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus required>
+                        <input type="text" name="nationality" id="nationality" class="validate" placeholder="Française"
+                            min="2" max="50" maxlength="50"
+                            pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus required>
                         <label for="nationality">Nationalité</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
@@ -56,7 +79,9 @@
                 <div class="row">
                     <div class="input-field col s12">
                         <i class="material-icons prefix">account_circle</i>
-                        <input type="email" name="email" id="email" class="validate" placeholder="johnDoe@mail.com" min="2" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus required>
+                        <input type="email" name="email" id="email" class="validate" placeholder="johnDoe@mail.com"
+                            min="4" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}"
+                            autofocus required>
                         <label for="email">E-mail</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
@@ -64,8 +89,30 @@
                 <div class="row">
                     <div class="input-field col s6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input type="text" name="nationality" id="nationality" class="validate" placeholder="Française" min="2" max="50" pattern="[a-zA-Zéèêëiîïôöüäç]{1,22}[-]?[a-zA-Zéèêëiîïôöüäç]{1,22}" autofocus required>
-                        <label for="nationality">Nationalité</label>
+                        <input type="tel" name="tel" id="tel" class="validate" placeholder="0608000000" maxlength="10"
+                            pattern="[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}" autofocus required>
+                        <label for="nationality">Tel.</label>
+                        <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                    </div>
+                    <div class="input-field col s6">
+                        <!-- <i class="material-icons prefix">account_circle</i> -->
+                        <select name="degree" id="degree">
+                            <option value="" disabled selected>Choisissez</option>
+                            <option value="Sans">Sans</option>
+                            <option value="Bac">Bac</option>
+                            <option value="Bac+2">Bac+2</option>
+                            <option value="Bac+3">Bac+3</option>
+                            <option value="Bac+4 et plus">Bac+4 et plus</option>
+                        </select>
+                        <label for="degree">Diplôme</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input type="text" name="idPoleEmploi" id="idPoleEmploi" maxlength="8" class="validate"
+                            placeholder="0342340N" pattern="[0-9]{7}[A-Z]{1}" required>
+                        <label for="email">Numéro Pôle Emploi</label>
                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
                 </div>
@@ -74,15 +121,53 @@
 
         </form>
     </section>
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var elems = document.querySelectorAll('.datepicker');
-            var options = {format:'yyyy-mm-dd'};
+
+<!-- Vérifie que materialize.js est bien chargé et initialise les champs de formulaire -->
+<script id="initMat">
+    setTimeout(importJS, 10);
+    // redirige vers page d'erreur si le script n'est pas chargé ou bloqué par le navigateur
+    tt.onerror = function(){
+        document.location.href='noScript.html';
+    };
+    // Initialise les champs quand materialise.js est chargé
+    tt.onload = function() {
+                var elems = document.querySelectorAll('.datepicker');
+                var options = {
+                firstDay: 1, //lundi en premier
+                format: 'dd-mm-yyyy',
+                maxDate: new Date(),
+                yearRange: [1900, new Date().getFullYear()], //année maxi : année en cours
+                selectMonths: true, //selecteur de mois
+                i18n: {
+                    nextMonth: '>',
+                    previousMonth: '<',
+                    months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août',
+                        'Septembre', 'Octobre', 'Novembre', 'Décembre'
+                    ],
+                    monthsShort: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aoû', 'Sep', 'Oct',
+                        'Nov', 'Déc'
+                    ],
+                    weekdays: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+                    weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+                    weekdaysAbbrev: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+                    clear: 'Réinitialiser',
+                    cancel: 'Annuler',
+                }
+
+            };
             var instances = M.Datepicker.init(elems, options);
-        });
-    </script>
+       
+                // Initialise le select de materialize
+
+            var elems = document.querySelectorAll('select');
+            var instances = M.FormSelect.init(elems);
+            
+    }
+
+
+
+    
+</script>
 
 </body>
 
